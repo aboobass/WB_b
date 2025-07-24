2e8797f52c2c30bb069f51edd2ef7dfb52a553a0
aboobass <ssaboobass@gmail.com>
2025-07-24 13:52:54 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ CONFIG_URL = "https://docs.google.com/spreadsheets/d/14yawg6EXbXus8R3Q9JvaZA4eQb
 CONFIG_SHEET_ID = '14yawg6EXbXus8R3Q9JvaZA4eQboX1vdN1GEFzZzwKJs'
 # токен бота
 # API_TOKEN = "8194459741:AAGx3YOGtQy96ZAJJiou9u4sUewLHIXScmQ"
-API_TOKEN = "7765052912:AAFeX7RxFtRy2hrnlWoaFn-eTgMp5th5pGk"
+API_TOKEN = "8194459741:AAGx3YOGtQy96ZAJJiou9u4sUewLHIXScmQ"
+# ADMIN_IDS = []  # ID админов через запятую
 ADMIN_IDS = [784291592]  # ID админов через запятую
 
 NOTIFY_ADMINS_ON_NEW_USER = True  # Включить/выключить уведомления
@@ -14,3 +15,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
           'https://www.googleapis.com/auth/drive']
 CREDS = Credentials.from_service_account_file(
     'credentials.json', scopes=SCOPES)
+
+
+YOOKASSA_TOKEN = "390540012:LIVE:74012"
+YOOKASSA_TEST_TOKEN = "381764678:TEST:132485"
